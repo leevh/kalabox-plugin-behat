f81936439851b72efa5a856b73ba02b9faff3034
Lee vanHalderen <lee.vanhalderen@viu.ca>
2017-07-12 14:48:29 -0700

    added elgalu/selenium for @javascript behat testing

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -18,6 +18,19 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * context constructor through behat.yml.
    */
   public function __construct() {
+    error_reporting(0);
+  }
+
+  /* Custom context below that will fill a field and test for autocomplete responses */
+  /**
+   * @When I write :text into autocomplete field :field
+   */
+  public function iWriteTextIntoAutocompleteField($text, $field)
+  {
+    $el = $this->getSession()->getDriver()->getWebDriverSession()->element("xpath", $this->getSession()->getSelectorsHandler()->selectorToXpath('named_exact', array('field', $field)));
+    $el->postValue(array('value' => array($text)));
+    //now wait for the data to load
+    sleep(5);
   }
 
 }
